b158d5950065e9bb1eb890227fcc9ceaf58eccf6
JoeIsHere <maugrimSlaya@gmail.com>
2025-03-10 14:23:50 -0700

    Updated yakbarber3 with watchdog, caching, and cleaned up code.

--- settings.py
+++ settings.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
+# This is the root directory of your site. If you're running this locally, it's probably just "./"
 root = "./"
-webRoot = "http://YOURDOMAIN"
+# This is the URL your site is hosted at. Use "https://" if your site has a SSL certificate.
+webRoot = "https://YOURDOMAIN"
+# This is the directory where your content is stored. It's relative to the root directory.
 contentDir = "content/"
+# This is the directory where your templates are stored. It's relative to the root directory.
 templateDir = root + "templates/default/"
+# This is the directory where your output will be stored. It's relative to the root directory.
 outputDir = root + "output/"
+# This is the title of your website.
 sitename = 'YOUR TITLE'
 # From the typekit "kit" get the 7 characters before the ".js"
 # If you're not using Adobe TypeKit, leave this blank and it will not process.
